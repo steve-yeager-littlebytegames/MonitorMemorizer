015628cec6ace8be1945056134ce2dd57348158e
Steve Yeager <steve.yeager@littlebytegames.com>
2020-12-16 19:27:27 -0800

    Project init

---
+++ Source/MonitorMemorizer/MonitorMemorizer.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonitorMemorizer.Cli
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
